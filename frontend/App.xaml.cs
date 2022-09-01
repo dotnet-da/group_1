6d37762fd91e55de24f83f756698edceee48d195
Tobias Abel <tobias.abel@stud.h-da.de>
2022-09-01 13:56:18 +0200

    added login page and resource files for later use in the application #16

--- frontend/App.xaml.cs
+++ frontend/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿using AccountsDLL.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,62 @@ namespace frontend
     /// </summary>
     public partial class App : Application
     {
+        public static HttpClient _accountsApi { get; set; }
+
+        public static Guid _userId { get; set; }
+        public static string _apiToken { get; set; }
+
+        public App()
+        {
+            // Get data from accounts API
+            _accountsApi = new HttpClient(new HttpClientHandler
+            {
+                UseProxy = false
+            });
+            _accountsApi.DefaultRequestHeaders.Accept.Clear();
+            _accountsApi.DefaultRequestHeaders.Accept.Add(
+                                 new MediaTypeWithQualityHeaderValue("application/json"));
+            _accountsApi.BaseAddress = new Uri("https://localhost:9595/api/accounts/");
+            //_accountsApi.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+        }
+
+        public static async Task<string> Login(string username, string password)
+        {
+            string message = "";
+            string path = "authenticate";
+
+            AuthenticateRequest authenticateRequest = new AuthenticateRequest
+            {
+                Username = username,
+                Password = password
+            };
+
+            var authenticateRequestContent = JsonConvert.SerializeObject(authenticateRequest);
+            var httpContent = new StringContent(authenticateRequestContent, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _accountsApi.PostAsync(path, httpContent);
+
+            if (!response.IsSuccessStatusCode && response.StatusCode != System.Net.HttpStatusCode.BadRequest)
+            {
+                message = "ERROR in login: " + response.StatusCode;
+                return message;
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            JObject joResponse = JObject.Parse(content);
+
+            if (joResponse["token"] != null && joResponse["id"] != null)
+            {
+                message = "Success";
+                _userId = (Guid)joResponse["id"];
+                _apiToken = (string)joResponse["token"];
+            }
+            else
+            {
+                message = (string)joResponse["message"];
+            }
+
+            return message;
+        }
     }
 }
---
+++ frontend/Login/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace frontend.Login
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordInput.Password.Length > 0)
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+                LoginButton.IsEnabled = false;
+
+                var status = await App.Login(UsernameInput.Text, PasswordInput.Password);
+
+                Mouse.OverrideCursor = null;
+                LoginButton.IsEnabled = true;
+
+                UsernameInput.Text = "";
+                PasswordInput.Password = "";
+                MessageBox.Show("Login status: " + status);
+            }
+            else
+            {
+                MessageBox.Show("Password is empty!");
+            }
+        }
+    }
+}
