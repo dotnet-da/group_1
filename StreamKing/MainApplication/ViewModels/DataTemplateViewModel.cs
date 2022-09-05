969aea816d96f2ae047091cd70eb1859a38d9883
Thai <quang.t.vu@stud.h-da.de>
2022-09-05 18:16:10 +0200

    DataTemplate

---
+++ StreamKing/MainApplication/ViewModels/DataTemplateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using StreamKing.Data.Media;
+using StreamKing.MainApplication;
+
+namespace StreamKing.MainApplication.ViewModels
+{
+    public class DataTemplateViewModel
+    {
+        public DataTemplateViewModel()
+        { 
+        }
+        public Media Media { get; set; }
+
+        public DataTemplateViewModel(Media media)
+        {
+            Media = media;
+            
+        }
+
+    }
+}
